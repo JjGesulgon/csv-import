763810a9221e572b2dbb9acf278cf796aba66718
jjgesulgon <jj@hybrain.co>
2020-12-05 00:25:42 +0800

    initial commit

---
+++ resources/js/views/Overview.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+      <div class="card-body">
+        <div class="container">
+          <div class="d-none d-sm-block display-4 px-3 font-weight-lighter text-center">Starting Template</div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    //
+  },
+  methods: {
+    // Add ES6 methods here
+  },
+  computed: {
+    // Add ES6 methods here that needs caching
+  },
+};
+</script>
